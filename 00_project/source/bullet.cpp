c5e2ea1496bffac28f7f940374ed1619ddab99f3
SakamotoKai <221093@st.yoshida-g.ac.jp>
2024-03-25 14:40:50 +0900

    弾完成、当たり判定追加

--- 00_project/source/bullet.cpp
+++ 00_project/source/bullet.cpp
@@ -30,15 +30,14 @@ namespace
 {
 	const int	PRIORITY		= 2;	// 弾の優先順位
 	const float SIZE_BULLET	= 50.0f;		// 半径
-	const int	LIFE = 10;					// 寿命
+	const int	LIFE = 5;					// 寿命
 	const D3DXCOLOR CLO = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);	//色
-	
 }
 
 //============================================================
 //	コンストラクタ
 //============================================================
-CBullet::CBullet() : CObject(CObject::LABEL_EFFECT, CObject::DIM_3D, PRIORITY),
+CBullet::CBullet() : CObject(CObject::LABEL_BULLET, CObject::DIM_3D, PRIORITY),
 	m_pos(VEC3_ZERO),	// 位置
 	m_move(VEC3_ZERO),	// 移動量
 	m_rot(VEC3_ZERO)	// 向き
@@ -79,14 +78,61 @@ void CBullet::Uninit(void)
 //============================================================
 void CBullet::Update(void)
 {
-	CEffect3D::Create(m_pos,
-		SIZE_BULLET,
-		CEffect3D::TYPE_BUBBLE,
-		LIFE, m_move,
+	m_move.y -= 0.98f;
+
+	m_pos += m_move;
+
+	CEffect3D::Create
+	(
+		m_pos,
+		m_Radius,
+		m_type,
+		LIFE,
+		VEC3_ZERO,
 		m_rot,
-		CLO,
-		0.0f,
-		CRenderState::BLEND_ADD);
+		CLO
+	);
+
+	CollisionFire();
+}
+
+//============================================================
+//	炎との当たり判定
+//============================================================
+void CBullet::CollisionFire(void)
+{
+	for (int nCntPri = 0; nCntPri < object::MAX_PRIO; nCntPri++)
+	{ // 優先順位の総数分繰り返す
+		// ポインタを宣言
+		CObject* pObjectTop = CObject::GetTop(DIM_3D, nCntPri);	// 先頭オブジェクト
+		if (pObjectTop != NULL)
+		{ // 先頭が存在する場合
+
+			// ポインタを宣言
+			CObject* pObjCheck = pObjectTop;	// オブジェクト確認用
+
+			while (pObjCheck != NULL)
+			{ // オブジェクトが使用されている場合繰り返す
+
+				// ポインタを宣言
+				CObject* pObjectNext = pObjCheck->GetNext();	// 次オブジェクト
+
+				if (pObjCheck->GetLabel() == CObject::LABEL_FIRE)
+				{ // オブジェクトラベルが地盤ではない場合
+
+					D3DXVECTOR3 posFire = pObjCheck->GetVec3Position();	// 敵位置
+
+					if (collision::Circle3D(m_pos, posFire, 25.0f, 25.0f) == true)
+					{
+						pObjCheck->Uninit();
+					}
+				}
+
+				// 次のオブジェクトへのポインタを代入
+				pObjCheck = pObjectNext;
+			}
+		}
+	}
 }
 
 //============================================================
@@ -102,9 +148,12 @@ void CBullet::Draw(CShader */*pShader*/)
 //============================================================
 CBullet* CBullet::Create
 (
-	const D3DXVECTOR3& rPos,	// 位置
-	const D3DXVECTOR3& rRot		// 向き
-)
+	const D3DXVECTOR3& rPos,		// 位置
+	const float			fRadius,	// 半径
+	const CEffect3D::EType	type,	// テクスチャ
+	const int			nLife,			// 寿命
+	const D3DXVECTOR3& rMove	// 移動量
+)	// 生成
 {
 	// ポインタを宣言
 	CBullet* pBullet = new CBullet;	// 弾情報
@@ -126,10 +175,16 @@ CBullet* CBullet::Create
 			return nullptr;
 		}
 
-		// 位置を設定
-		pBullet->SetVec3Position(rPos);
+		//// 位置を設定
+		//pBullet->SetVec3Position(rPos);
 
-		pBullet->SetVec3Rotation(rRot);
+		//pBullet->SetVec3Rotation(VEC3_ZERO);
+
+		pBullet->m_pos = rPos;
+		pBullet->m_Radius = fRadius;
+		pBullet->m_type = type;
+		pBullet->m_nLife = nLife;
+		pBullet->m_move = rMove;
 
 		// 確保したアドレスを返す
 		return pBullet;
